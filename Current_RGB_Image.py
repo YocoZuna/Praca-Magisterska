8101ca0edf06620b851be0b999ef2003c2ad0c04
MrWisnia0404 <mati0404@onet.pl>
2023-07-05 19:45:10 +0200

    Update Current_RGB_Image.py

--- Current_RGB_Image.py
+++ Current_RGB_Image.py
@@ -16,45 +16,42 @@ count = 0
 lista = [ ]
 listsize = 0
 temp = 0
+PathToFile = "Data_test_1024_samples.txt"
 
 cm = Colormap()
 my_cmap_red = cm.cmap_bicolor('black', 'red')
 my_cmap_green = cm.cmap_bicolor('black', 'green')
 my_cmap_blue = cm.cmap_bicolor('black', 'blue')
 
-PathToFile = "ReadImu.csv"
-buffor= [ ]
 try:
-    dataset = open(PathToFile,'r').readline()
+    dataset = open(PathToFile,'r').readlines()
     listsize = len(dataset)
-    
 
 except:
     print("Could not open CSV file\n")
-
-
-buffor = dataset.split(',')
-
+    
+temp = 0
 for i in range(0,(int(listsize/256))):
     
     lista.append(temp)
     temp = temp +256
 
 
+
 lenth = len(lista)
 iterration= 0
 while iterration <= lenth:
     try:
-       
+        dataset = open(PathToFile,'r').readlines()
         temp1 = lista[iterration]
         temp2 = lista[iterration+1]
-        datasett = buffor[temp1:temp2]
+        dataset = dataset[temp1:temp2]
     except:
         break
     
     #lines = dataset.split("\n")
     iterration+=1
-    lines = datasett
+    lines = dataset
     
     t = []
     ax=[]
@@ -68,51 +65,70 @@ while iterration <= lenth:
     pb =[]
     pc = []
 
-
-    for line in lines:
+    for line in dataset:
         if len(line) > 1:
             line = line[3:len(line)-3]
-          
+            
             sample,aax,aay,aaz,xx,yy,zz,phasea,phaseb,phasec = line.split(',')
             t.append(float(sample))
             ax.append(float(aax)-0.1044676)
             ay.append(float(aay)+0.0477295)
             az.append(float(aaz)-0.9561013)
 
-            gx.append(float(xx)+4.5992367)
-            gy.append(float(yy)+43.36626)
-            gz.append(float(zz)-1.5074805)
+            x.append(float(xx)+4.5992367)
+            y.append(float(yy)+43.36626)
+            z.append(float(zz)-1.5074805)
             
             pa.append(float(phasea))
             pb.append(float(phaseb))
             pc.append(float(phasec))
             
-       
-    axyfft = rfft(gx)
-    axfft = rfftfreq(len(gx), 1/500)
 
-    ayyfft = rfft(gy)
-    ayfft = rfftfreq(len(gy), 1/500)
 
-    azyfft = rfft(gz)
-    azfft = rfftfreq(len(gz), 1/500)
-   
 
-    print(f" Time_Start {t[0]}\n Time_End {t[-1]}\n")
+
+
+        # For Sine Function
+        """    axis[0, 0].plot(t, ax)
+            
+            plt.ylim(-1,1)
+            axis[0, 0].set_title("Ax")
+            #plt.savefig('my_plot.png')
+            axis[1, 0].plot(t, ay)
+            plt.ylim(-1,1)
+            axis[1, 0].set_title("Ay")
+            #plt.savefig('my_plot.png')
+            axis[2, 0].plot(t, az)
+            plt.ylim(-1,1)
+            axis[2, 0].set_title("Az")
+            #plt.savefig('my_plot.png')
+            # For Cosine Function
+            axis[0, 1].plot(t,x)
+            plt.ylim(-1,1)
+            axis[0, 1].set_title("Gx")
+            #plt.savefig('my_plot.png')
+            axis[1, 1].plot(t, y)
+            plt.ylim(-1,1)
+            axis[1, 1].set_title("Gy")
+            #plt.savefig('my_plot.png')
+            axis[2, 1].plot(t, z)
+            plt.ylim(-1,1)
+            axis[2, 1].set_title("Gz")
+            #plt.savefig('my_plot.png')
+            plt.ylim(-1,1)
+        """
+
     plt.figure(1)
-    #plt.figure(figsize=(150,75)) ### Re-size (2,2) means 200x200 pixels .. Also wie have to sue plt.axis('off') to turn of the axis lines
-    #plt.axis('off')
-    plt.title("Acc")
-    #plt.plot(axfft, np.abs(axyfft))
-    plt.plot(ayfft, np.abs(ayyfft))
-    #plt.plot(azfft, np.abs(azyfft))
-    plt.legend(['Ax','Ay','Az'])
- 
+    plt.plot(t, pa,'r')
 
-    plt.ylabel('Frequency (Hz)');
-    plt.xlabel('Time (s)');
+    #plt.plot(t,y,'b')
+    #plt.plot(t,z,'g')
     plt.show()
-    plt.figure(1).clear()
-    plt.figure(2).clear()
-    plt.figure(3).clear()
-    plt.figure(4).clear()
+
+
+    
+    #plt.ylim(-17,17)
+        
+    #os.remove("C:\\Users\\dawid\\Desktop\\Python\\test.csv")
+    
+    # Creating vectors X and Y where Y is predicted value X input values  
